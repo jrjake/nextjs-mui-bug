dcd8ce9d082d2b81104bb8c3b1f6869380857a2b
VillageR88 <villager88pl@gmail.com>
2024-03-22 09:50:30 +0100

    Box fix
    Signed-off-by: VillageR88 <villager88pl@gmail.com>

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,11 +1,16 @@
-import {Box, CssBaseline} from "@mui/material";
-import {AppRouterCacheProvider} from "@mui/material-nextjs/v14-appRouter";
-import {ThemeProvider} from "@mui/material/styles";
+import dynamic from "next/dynamic";
+import { CssBaseline } from "@mui/material";
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v14-appRouter";
+import { ThemeProvider } from "@mui/material/styles";
+
 
 export default function RootLayout({ children }) {
   // This is {} when "type": "module" in package.json, but [Function (anonymous)] otherwise
   // Naturally, using this {} throws an error: "Error: Element type is invalid. Received a promise that resolves to: [object Module]. Lazy element type must resolve to a class or function."
-  console.log(Box);
+  const Box = dynamic(() => import('@mui/material/Box'), {
+    ssr: false,
+  });
+  //console.log(Box);
 
   return (
     <html lang="en">
